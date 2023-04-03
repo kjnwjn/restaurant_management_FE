4329318e50fd839958fd9940dec0997a801fa1c7
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-03 19:19:26 +0700

    Update

--- src/components/CardVue.vue
+++ src/components/CardVue.vue
@@ -1,13 +1,38 @@
 <template>
-    <div class="card-app"></div>
+    <div class="card-app-wrapper p-6">
+        <div id="card-app" :class="{ active: status }">
+            <div class="card-app-img p-6">
+                <img src="@/assets/plate.png" width="100%" />
+            </div>
+            <p>{{ tableId }}</p>
+            <p>{{ status ? "Unavailable" : "Available" }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
-        msg: String,
+        tableId: String,
+        status: Boolean,
     },
 };
 </script>
 
-<style></style>
+<style>
+#card-app {
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    background: white;
+    transition: all 0.2s linear;
+    box-shadow: 0px 3px 9px -2px rgba(0, 0, 0, 0.4);
+}
+#card-app.active {
+    background: #b5b5b5;
+}
+#card-app:hover {
+    transform: scale(1.1);
+    box-shadow: 0px 3px 9px 2px rgba(0, 0, 0, 0.4);
+}
+</style>
